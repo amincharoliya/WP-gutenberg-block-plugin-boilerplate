b8ca68f46c3bbfea4af508bd9de63fb6c7a66403
Amin Charoliya <amincharoliya@gmail.com>
2022-01-31 02:13:33 +0530

    Rename block

---
+++ block.js
@@ -0,0 +1,42 @@
+const { PlainText } = wp.editor;
+const { registerBlockType } = wp.blocks;
+
+//Import Styles
+import "./style.css";
+import "./editor.css";
+
+registerBlockType('accordion-block/main', {   
+    title: 'Accordion Block',
+    icon: 'menu',
+    category: 'common',
+
+    attributes: {
+
+        title: {
+          source: 'text',
+          selector: '.accordion__title'
+        },
+
+    },
+
+    edit( { attributes, setAttributes, className  } ) {
+
+        return (
+            <PlainText
+                onChange={ content => setAttributes({ title: content }) }
+                value={ attributes.title }
+                placeholder="Enter title"
+                className="heading"
+            />
+        )
+
+    },
+
+    save( { attributes } ) {
+
+        return (
+            <h2 className="accordion__title">{ attributes.title }</h2>
+        )
+    }
+
+});
